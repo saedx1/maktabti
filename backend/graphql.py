969eddc2bc85a75b2b5a0f90a21265bd40e597f3
saedx1 <saadmtsa@gmail.com>
2021-09-05 16:47:29 -0400

    put graphql into a separate file

---
+++ backend/graphql.py
@@ -0,0 +1,18 @@
+import json
+import os
+import requests
+
+
+GRAPHQL_ENDPOINT = os.environ.get(
+    "GRAPHQL_ENDPOINT", "http://localhost:8080/v1/graphql"
+)
+
+
+def execute_graphql_query(query, variables=None):
+    response = requests.post(
+        GRAPHQL_ENDPOINT, data=json.dumps({"query": query, "variables": variables})
+    )
+    if response.text is None:
+        return response.text
+
+    return json.loads(response.text)
