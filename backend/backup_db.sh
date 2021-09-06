079105a5219375b5baf75a9c463dcd4d983b2450
saedx1 <saadmtsa@gmail.com>
2021-09-06 12:24:31 -0400

    change backup to be hourly

--- backend/backup_db.sh
+++ backend/backup_db.sh
@@ -2,5 +2,5 @@ source hasura-cloud/.env
 while true; do
 PGPASSWORD=$PGPASSWORD pg_dump -d postgres -h localhost -U postgres > backup.sql
 aws s3 cp backup.sql s3://maktabti-backups/database/$(date +%s).sql --profile maktabti
-sleep 6h
+sleep 1h
 done
