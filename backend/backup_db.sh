6958f6acefa0b8d3d5295e84d1f0a804c0af90ab
saedx1 <saadmtsa@gmail.com>
2021-09-05 21:33:14 -0400

    adjust host name for backup script

--- backend/backup_db.sh
+++ backend/backup_db.sh
@@ -1,6 +1,6 @@
 source .env
 while true; do
-pg_dump -d postgres -h mindz -U postgres > backup.sql
+pg_dump -d postgres -h localhost -U postgres > backup.sql
 aws s3 cp backup.sql s3://maktabti-backup/database/$(date +%s).sql
 sleep 6h
 done
