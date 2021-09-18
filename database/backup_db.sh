ff46c688aba0921c7ef722b7230ce9544b1331a5
Saed SayedAhmed <saadmtsa@gmail.com>
2021-09-17 22:51:04 -0400

    adjust database backup script and expose containers only locally

--- database/backup_db.sh
+++ database/backup_db.sh
@@ -1,4 +1,4 @@
-source hasura-cloud/.env
+source ./.env
 while true; do
 PGPASSWORD=$PGPASSWORD pg_dump -d postgres -h localhost -U postgres > backup.sql
 aws s3 cp backup.sql s3://maktabti-backups/database/$(date +%s).sql --profile maktabti
